bbb23af5df948f92407576550df59c0f6a3ff9c6
OVHSRobotics4619 <OVHSRobotics@gmail.com>
2025-02-22 10:53:59 -0800

    Wheels aligned, should turn left and right appropriately...next is PID

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,7 +40,7 @@ public class RobotContainer {
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
       () -> m_driverController.getLeftY() * -1,
       () -> m_driverController.getLeftX() * -1)
-      .withControllerRotationAxis(() -> m_driverController.getRightX() * -1)
+      .withControllerRotationAxis(() -> m_driverController.getRightX() * 1)
       .deadband(OperatorConstants.DEADBAND)
       .scaleTranslation(0.8)
       .allianceRelativeControl(true);
